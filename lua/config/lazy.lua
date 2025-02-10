88e91574fb3dbf6dceafdb945f4f252686ccf5ba
Matthias Derer <matthias.derer@esono.de>
2025-02-10 23:01:00 +0100

    add harpoon and integrate it with fzf

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -52,6 +52,63 @@ require("lazy").setup({
   },
 })
 
+-- harpoon
+
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set("n", "<leader>a", function()
+  harpoon:list():add()
+end)
+-- vim.keymap.set("n", "<C-e>", function()
+--   harpoon.ui:toggle_quick_menu(harpoon:list())
+-- end)
+--
+local fzf = require("fzf-lua")
+local fzf_actions = require("fzf-lua.actions")
+local function harpoon_fzf_picker(harpoon_files)
+  local entries = {}
+  for _, item in ipairs(harpoon_files.items) do
+    table.insert(entries, item.value)
+  end
+
+  fzf.fzf_exec(entries, {
+    previewer = "builtin",
+    prompt = "Harpoon > ",
+    actions = {
+      ["default"] = function(selected)
+        fzf_actions.file_edit(selected, {})
+      end,
+      ["ctrl-x"] = function(selected)
+        local files = harpoon:list()
+        for _, item in ipairs(files.items) do
+          if item.value == selected[1] then
+            harpoon:list():remove(item)
+            harpoon_fzf_picker(files)
+            break
+          end
+        end
+      end,
+    },
+  })
+end
+
+vim.keymap.set("n", "<C-e>", function()
+  harpoon_fzf_picker(harpoon:list())
+end, { desc = "Harpoon FZF" })
+
+-- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+-- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+-- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+-- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+--
+-- Toggle previous & next buffers stored within Harpoon list
+-- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+-- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+
 -- require("lspconfig").intelephense.setup({
 --   settings = {
 --     intelephense = {
--- lua/plugins/additional.lua
+++ lua/plugins/additional.lua
@@ -8,4 +8,9 @@ return {
   -- {
   --   "ThePrimeagen/vim-be-good",
   -- },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
 }
