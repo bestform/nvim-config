c7fcc0db5ce0f69dfad92ef0a6877e1b282e7f74
Matthias Derer <matthias.derer@esono.de>
2025-08-06 12:14:25 +0200

    remove auto save for now
    the autocmd conflicts somehow if an oil buffer is open in the background
    I must investigate this further. But today is not that day.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,15 +2,16 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-local group_prefix = "bestform_"
-
-vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
-  group = vim.api.nvim_create_augroup(group_prefix .. "auto-save", { clear = true }),
-  callback = function()
-    -- acwrite is the filetype of the oil buffer
-    if vim.o.buftype ~= "nofile" and vim.o.buftype ~= "acwrite" then
-      vim.cmd("!silent write")
-      -- print("saved")
-    end
-  end,
-})
+-- local group_prefix = "bestform_"
+--
+-- vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+--   group = vim.api.nvim_create_augroup(group_prefix .. "auto-save", { clear = true }),
+--   callback = function(ev)
+--     -- acwrite is the filetype of the oil buffer
+--     if vim.bo[ev.buf].buftype ~= "nofile" and vim.bo[ev.buf].buftype ~= "acwrite" then
+--       vim.cmd("!silent write")
+--       print("saved")
+--     end
+--     return true
+--   end,
+-- })
