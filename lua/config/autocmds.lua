a04453db9b41fffa05385a47e2d2cedce7a2bc1d
Matthias Derer <matthias.derer@esono.de>
2025-08-06 11:17:25 +0200

    add autosave as autocommand

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local group_prefix = "bestform_"
+
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  group = vim.api.nvim_create_augroup(group_prefix .. "auto-save", { clear = true }),
+  callback = function()
+    -- acwrite is the filetype of the oil buffer
+    if vim.o.buftype ~= "nofile" and vim.o.buftype ~= "acwrite" then
+      vim.cmd("!silent write")
+      -- print("saved")
+    end
+  end,
+})
