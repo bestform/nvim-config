70a434eaf98860ef5313d8fc9e285fb355721c7f
Matthias Derer <matthias.derer@haufe-akademie.de>
2025-10-06 16:45:20 +0200

    remove oil

--- lua/plugins/additional.lua
+++ lua/plugins/additional.lua
@@ -113,17 +113,6 @@ return {
   {
     "NMAC427/guess-indent.nvim", -- Detect tabstop and shiftwidth automatically
   },
-  {
-    "stevearc/oil.nvim",
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {},
-    -- Optional dependencies
-    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-    lazy = false,
-  },
   { "dmmulroy/ts-error-translator.nvim" },
   {
     "Wansmer/treesj",
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -4,6 +4,17 @@ return {
     enabled = false,
   },
   {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
+  {
     "sphamba/smear-cursor.nvim",
     enabled = false,
     opts = { -- Default  Range
